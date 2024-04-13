f58c4e0649f2f92b34f542bba53a8c4172052e55
Sofiia <hentaionelove2005@gmail.com>
2024-04-13 15:54:53 +0300

    cards, descriptions for cards and sounds update

--- src/CyberTarot/PlaySound.js
+++ src/CyberTarot/PlaySound.js
@@ -1,24 +1,32 @@
 const sounds = {
-    buttonClick: new Audio('sounds/social_button_pressing.mp3'),
-    cardShow: new Audio('sounds/open_card_sound.mp3'),
-    button_hovering: new Audio('sounds/button_hovering.mp3'),
-    opening_manual: new Audio('sounds/opening_manual.mp3'),
-    close_manual: new Audio('sounds/close_manual.mp3'),
-    mode_change: new Audio('sounds/mode_change.mp3')
+    buttonClick: new Audio('sounds/Social_button_pressing.mp3'),
+    button_hovering: new Audio('sounds/Button_hovering.mp3'),
+    opening_manual: new Audio('sounds/Opening_manual.mp3'),
+    close_manual: new Audio('sounds/Close_manual.mp3'),
+    mode_change: new Audio('sounds/Mode_change.mp3')
 }
 
 function socialButtons() {
+    sounds.buttonClick.volume = 0.5;
     sounds.buttonClick.play();
 }
 
-function cardsShowing() {
-    sounds.cardShow.play();
-}
-
 function buttonClick(){
+    sounds.button_hovering.volume = 0.5;
     sounds.button_hovering.play();
 }
 
-//document.getElementById('gitHub-button').addEventListener('click', socialButtons);
-//document.getElementById('contacts-button').addEventListener('click', socialButtons);
-//document.getElementById('back-to-menu').addEventListener('click', buttonClick);
+function openingManual(){
+    sounds.opening_manual.volume = 0.5;
+    sounds.opening_manual.play();
+}
+
+function closingManual(){
+    sounds.close_manual.volume = 0.5;
+    sounds.close_manual.play();
+}
+
+document.getElementById('gitHub-button').addEventListener('click', socialButtons);
+document.getElementById('contacts-button').addEventListener('click', socialButtons);
+document.getElementById('back-to-menu').addEventListener('click', buttonClick);
+document.getElementById('manual').addEventListener('click', openingManual);
